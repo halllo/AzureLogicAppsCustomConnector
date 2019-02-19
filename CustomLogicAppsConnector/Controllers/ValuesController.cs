e6da22c88016f42ada4294e92d807c0f35779ca6
Manuel Naujoks <mnaujoks@gmail.com>
2019-02-19 15:31:11 +0100

    sample app

---
+++ CustomLogicAppsConnector/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomLogicAppsConnector.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		public static List<string> values = new List<string>(new string[] { "value1", "value2" });
+
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> GetsAllTheValues()
+		{
+			return values;
+		}
+
+		//// GET api/values/5
+		//[HttpGet("{id}")]
+		//public ActionResult<string> Get(int id)
+		//{
+		//	return values[id];
+		//}
+
+		// POST api/values
+		[HttpPost]
+		public void AddsANewValue([FromBody] string value)
+		{
+			values.Add(value);
+		}
+
+		//// PUT api/values/5
+		//[HttpPut("{id}")]
+		//public void Put(int id, [FromBody] string value)
+		//{
+		//	values[id] = value;
+		//}
+
+		//// DELETE api/values/5
+		//[HttpDelete("{id}")]
+		//public void Delete(int id)
+		//{
+		//	values.RemoveAt(id);
+		//}
+	}
+}
---
+++ CustomLogicAppsConnector/Controllers/WebhookTriggerController.cs
@@ -0,0 +1,41 @@
+﻿namespace CustomLogicAppsConnector.Controllers
+{
+	//[Route("api/webhooktrigger")]
+	//[ApiController]
+	//public class WebhookTriggerController : ControllerBase
+	//{
+	//	public static List<string> subscriptions = new List<string>();
+
+
+	//	[HttpPost, Route("subscribe")]
+	//	public ActionResult Subscribe([FromBody] string callbackUrl)
+	//	{
+	//		subscriptions.Add(callbackUrl);
+
+	//		return Accepted();
+	//	}
+
+
+	//	[HttpGet, Route("trigger")]
+	//	public async Task<ActionResult> Get()
+	//	{
+	//		using (var client = new HttpClient())
+	//		{
+	//			foreach (string callbackUrl in subscriptions)
+	//			{
+	//				await client.PostAsync(callbackUrl, @"{""trigger"":""fired""}", new JsonMediaTypeFormatter(), "application/json");
+	//			}
+	//		}
+
+	//		return Accepted(new { SubscriptionsFired = subscriptions.Count });
+	//	}
+
+
+	//	[HttpPost, Route("unsubscribe")]
+	//	public ActionResult Unsubscribe([FromBody] string callbackUrl)
+	//	{
+	//		subscriptions.Remove(callbackUrl);
+	//		return Accepted();
+	//	}
+	//}
+}
